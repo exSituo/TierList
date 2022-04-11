21b003c1f37329836b8e71307211ea63cfc0d5a0
qjoly <quentinjoly@thoughtless.eu>
2022-04-11 15:02:13 +0200

    Nouveau package: Metier

---
+++ src/metier/Anime.java
@@ -0,0 +1,25 @@
+package metier;
+
+public class Anime
+{
+    private String nom;
+
+    public String getNom()
+    {
+        return nom;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    private String url;
+
+    private Integer id;
+}
