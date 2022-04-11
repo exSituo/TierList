c24a327d0572447800c7aec4161fdf34a81518fe
Stagiaire ENFiP <popblue_sky@outlook.fr>
2022-04-11 16:05:26 +0200

    ADD méthodes SERVICES

--- src/dao/Anime.java
+++ src/dao/Anime.java
@@ -1,5 +1,16 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import metier.Classement;
+import metier.User;
+
 public class Anime
 {
     private static Anime instance = new Anime();
@@ -15,7 +26,64 @@ public class Anime
     }
     
     /*
-     * Transaction : chargement de l'animé ?
+     * Transaction : permet l'affichage des données
+     * renvoie les données du classement de user à partir de la base de données  
+     * accès via idUser
+     * Key --> idUser Value --> score classement
      */
+   
+    public Map<Integer, Classement>  initClassementUser (Transaction transaction)
+    {
+        Map<Integer, Classement> resultat = new HashMap<>();
+
+        try
+        {
+            Connection c = transaction.getConnexion();
+            String requete = "insert  from "; 
+
+           
+        }
+        catch (Exception e)
+        {
+            Logger.getLogger(Anime.class).error(e.getMessage());
+        }
+        
+        return null;
+        
+    }
+    
+    /*
+     * Transaction : chargement  du classement de user dans la base de données  
+     * Key --> idUser Value --> score classement
+     */
+   
+    public Map<Integer, Classement> affClassementUser (Transaction transaction, User idUser)
+    {
+        Map<Integer, Classement> resultat = new HashMap<>();
+
+        try
+        {
+            Connection c = transaction.getConnexion();
+            String requete = "select  from "; // à modifier !
+            try (PreparedStatement st = c.prepareStatement(requete);
+                ResultSet rst = st.executeQuery())
+            {
+                while (rst.next())
+                {
+                    //Ce qui est renvoyé ! 
+                }
+            }
+            
+   
+        }
+        catch (Exception e)
+        {
+            Logger.getLogger(Anime.class).error(e.getMessage());
+        }
+        
+        return null;
+        
+    }
+    
     
 }
